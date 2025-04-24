7a9f181bb04c5de5411527001ac64bde297ca979
Sgheza8726 <98853989+Sgheza8726@users.noreply.github.com>
2025-04-24 19:35:22 -0400

    Update Report.java

--- src/final/question3/Report.java
+++ src/final/question3/Report.java
@@ -1,5 +1,51 @@
 package org.howard.edu.lspfinal.question3;
 
-public class Report {
+abstract class Report {
+    public final void generateReport() {
+        loadData();
+        formatData();
+        printReport();
+    }
 
+    protected abstract void loadData();
+    protected abstract void formatData();
+    protected abstract void printReport();
+}
+
+class SalesReport extends Report {
+    protected void loadData() {
+        System.out.println("Loading sales data...");
+    }
+
+    protected void formatData() {
+        System.out.println("Formatting sales data...");
+    }
+
+    protected void printReport() {
+        System.out.println("Printing sales report.\n");
+    }
+}
+
+class InventoryReport extends Report {
+    protected void loadData() {
+        System.out.println("Loading inventory data...");
+    }
+
+    protected void formatData() {
+        System.out.println("Formatting inventory data...");
+    }
+
+    protected void printReport() {
+        System.out.println("Printing inventory report.\n");
+    }
+}
+
+public class Driver {
+    public static void main(String[] args) {
+        Report sales = new SalesReport();
+        Report inventory = new InventoryReport();
+
+        sales.generateReport();
+        inventory.generateReport();
+    }
 }
