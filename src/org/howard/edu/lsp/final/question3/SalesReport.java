9fd0081c6f82246b49921096428c63dbd47046b3
Sgheza8726 <98853989+Sgheza8726@users.noreply.github.com>
2025-04-24 20:38:27 -0400

    updated SalesReport.java

---
+++ src/org/howard/edu/lsp/final/question3/SalesReport.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.final.question3;
+
+public class SalesReport extends Report {
+    protected void loadData() {
+        System.out.println("Loading sales data...");
+    }
+
+    protected void formatData() {
+        System.out.println("Formatting sales data...");
+    }
+
+    protected void printReport() {
+        System.out.println("Printing sales report.");
+    }
+}
