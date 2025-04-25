e48a6354559436bce9061fd0ef82d6051abe7f18
Sgheza8726 <98853989+Sgheza8726@users.noreply.github.com>
2025-04-24 20:36:24 -0400

    updated TaskNotFoundException.java

---
+++ src/org/howard/edu/lsp/final/question2/TaskNotFoundException.java
@@ -0,0 +1,7 @@
+package org.howard.edu.lsp.final.question2;
+
+public class TaskNotFoundException extends Exception {
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+}
