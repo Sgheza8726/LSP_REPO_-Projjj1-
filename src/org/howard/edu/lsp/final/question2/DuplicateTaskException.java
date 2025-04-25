32e8919ee968d6fcf0cc05816570da9785f2d394
Sgheza8726 <98853989+Sgheza8726@users.noreply.github.com>
2025-04-24 20:35:34 -0400

    updated DuplicateTaskException.java

---
+++ src/org/howard/edu/lsp/final/question2/DuplicateTaskException.java
@@ -0,0 +1,7 @@
+package org.howard.edu.lsp.final.question2;
+
+public class DuplicateTaskException extends Exception {
+    public DuplicateTaskException(String message) {
+        super(message);
+    }
+}
