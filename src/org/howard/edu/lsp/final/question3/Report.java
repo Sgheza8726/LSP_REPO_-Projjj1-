df501de6a72600fbd965bb4eaea947d53c50aa16
Sgheza8726 <98853989+Sgheza8726@users.noreply.github.com>
2025-04-24 20:37:53 -0400

    Update Report.java

--- src/org/howard/edu/lsp/final/question3/Report.java
+++ src/org/howard/edu/lsp/final/question3/Report.java
@@ -1,6 +1,6 @@
-package org.howard.edu.lspfinal.question3;
+package org.howard.edu.lsp.final.question3;
 
-abstract class Report {
+public abstract class Report {
     public final void generateReport() {
         loadData();
         formatData();
@@ -10,42 +10,4 @@ abstract class Report {
     protected abstract void loadData();
     protected abstract void formatData();
     protected abstract void printReport();
-}
-
-class SalesReport extends Report {
-    protected void loadData() {
-        System.out.println("Loading sales data...");
-    }
-
-    protected void formatData() {
-        System.out.println("Formatting sales data...");
-    }
-
-    protected void printReport() {
-        System.out.println("Printing sales report.\n");
-    }
-}
-
-class InventoryReport extends Report {
-    protected void loadData() {
-        System.out.println("Loading inventory data...");
-    }
-
-    protected void formatData() {
-        System.out.println("Formatting inventory data...");
-    }
-
-    protected void printReport() {
-        System.out.println("Printing inventory report.\n");
-    }
-}
-
-public class Driver {
-    public static void main(String[] args) {
-        Report sales = new SalesReport();
-        Report inventory = new InventoryReport();
-
-        sales.generateReport();
-        inventory.generateReport();
-    }
 }
